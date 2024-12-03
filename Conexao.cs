60965867e44eebf027539795f838e16e682b9a1b
fabsferreira <145173673+fabsferreira@users.noreply.github.com>
2024-12-02 23:33:07 -0300

    Add files via upload

---
+++ Conexao.cs
@@ -0,0 +1,26 @@
+﻿namespace PlatinumDriverAutos.Models;
+ using MySql.Data.MySqlClient;
+
+    public class Conexao
+    {
+       
+        static void Main(string[] args)
+        {
+            string connectionString = "Server=localhost;Database=platinumdrive;User=root;Password=;Admin";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    Console.WriteLine("Conexão bem-sucedida com o banco de dados!");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Erro ao conectar: " + ex.Message);
+                }
+            }
+        }
+    }
+
+
